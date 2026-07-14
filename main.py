162a4b44c18c685d071cabaea3841c55aa41325f
pramudya-27 <dio2431058@itpln.ac.id>
2026-07-14 14:56:12 +0700

    Stage 0: hello server

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_server():
+    return {"message": "Hello, server!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
