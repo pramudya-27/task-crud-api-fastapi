9e81bc5331b0ecc38a5bd2b314b0698efa6a30aa
pramudya-27 <dio2431058@itpln.ac.id>
2026-07-23 20:11:56 +0700

    Stage 3: switch repository to PostgreSQL

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,24 @@
-from app.repositories.sqlite import SQLiteTaskRepository
+import os
+
+from dotenv import load_dotenv
+
+from app.repositories.postgres import PostgresTaskRepository
 from app.services.task_service import TaskService
 
 
-repository = SQLiteTaskRepository(
-    database_path="tasks.db",
+load_dotenv()
+
+
+database_url = os.getenv("DATABASE_URL")
+
+if not database_url:
+    raise RuntimeError(
+        "DATABASE_URL is not configured"
+    )
+
+
+repository = PostgresTaskRepository(
+    database_url=database_url,
 )
 
 task_service = TaskService(
